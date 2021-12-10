ee4ea9fa9332d037bccc2942fb0202a31ef5fa42
kleitonalbuquerque <kleiton2102@gmail.com>
2021-12-10 17:25:49 -0300

    Add bootstrap ao projeto

--- index.js
+++ index.js
@@ -5,26 +5,8 @@ const port = 4000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/:nome?/:lang?", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMsg = false;
-  var produtos = [
-    { nome: "Doritos", preco: 5.99 },
-    { nome: "Coca-Cola", preco: 7.99 },
-    { nome: "M&Ms", preco: 6.99 },
-    { nome: "Charge", preco: 2.99 },
-    { nome: "Red Bull", preco: 8.99 },
-  ];
-
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresas: ["IK Solution", " Mirante"],
-    // empresas: "",
-    msg: exibirMsg,
-    produtos: produtos,
-  });
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.listen(port, (error) => {
